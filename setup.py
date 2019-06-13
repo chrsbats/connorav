6c1f1c30b5050168658cc5ba122651b68d3e1340
Maximiliano Greco <mgreco@ets.es>
2019-06-13 13:33:15 +0200

    BLD: Now it use setup tools and find_packages

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='connorav',
     version='0.1',
     author='C Bates',
     author_email='chrsbats@gmail.com',
-    packages=['connorav'],
-    scripts=[],
+    packages=find_packages(),
     url='https://github.com/chrsbats/connorav',
     license='LICENSE.TXT',
     description='Fast Generation of Correlated Non-Normal Random Variates',
-    long_description='Fast Generation of Correlated Non-Normal Random Variates.  Define your distributions by mean, std, skew and kurtosis and a correlation matrix. Useful for Monte-Carlo testing',
+    long_description='Fast Generation of Correlated Non-Normal Random Variates. Define your distributions by mean, std, skew and kurtosis and a correlation matrix. Useful for Monte-Carlo testing',
     install_requires=[
-        "numpy>=1.9.0", "scipy>=0.14.0"
+        "numpy>=1.9.0",
+        "scipy>=0.14.0"
     ],
 )
